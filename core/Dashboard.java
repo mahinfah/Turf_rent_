987c9c41d9afbfbeb3c861d360432d22f6bd779f
mahinfah <mahinfah1234@gmail.com>
2024-05-27 02:21:19 -0800

    laptop cng

--- core/Dashboard.java
+++ core/Dashboard.java
@@ -1,5 +1,13 @@
 package core;
+import javax.swing.*;
 
-public class Dashboard {
+public class Dashboard 
+{
+    private JFrame f;
+    private JButton dark_mode;
+    private JLabel name , pass , title ;
+    private JTextField name_field ;
+    private JPasswordField pass_field;
+    private JButton enter ;
     
 }
